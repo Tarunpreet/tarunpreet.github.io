d48e59d5e17f81d9da79e868b437ce8c40ebcd5b
TARUNPREET <tarunsuri2697@gmail.com>
2018-04-24 12:34:30 +0530

    updated

--- IPU/server.js
+++
@@ -1,6 +0,0 @@
-var express=require('express');
-var app=express();
-var port=3001;
-app.use(express.static(__dirname+'/'));
-app.listen(port);
-console.log(' Server Started by Tarunpreet singh At '+port);
